d565eb6730e0721fc2508f57115e38f2fcae212d
Camba <fercambafcm@gmail.com>
2024-04-25 17:38:58 -0600

    Update

--- source/Reg.js
+++ source/Reg.js
@@ -1,50 +1,44 @@
-const formulario = document.getElementById('formulario')
-const inputs = document.querySelectorAll('#formulario input')
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    cp: /^\d{5,5}$/, // 5 a 5 numeros.
-    telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-    lc: /^\d{4,7}$/,
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
 
 const campos = {
 	usuario: false,
-    nombre: false,
-    cp: false,
-    telefono: false,
-    lc: false,
+	nombre: false,
 	password: false,
-    correo: false,
+	correo: false,
+	telefono: false
 }
 
 const validarFormulario = (e) => {
-    switch (e.target.name){
-        case "usuario":
-            cons
-        break;
-        case "nombre":
-            cons
-        break;
-        case "cp":
-            cons
-        break;
-        case "telefono":
-            cons
-        break;
-        case "lc":
-            cons
-        break;
-        case "password":
-            cons
-        break;
-        case "correo":
-            cons
-        break;
-    } 
+	switch (e.target.name) {
+		case "usuario":
+			validarCampo(expresiones.usuario, e.target, 'usuario');
+		break;
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "password":
+			validarCampo(expresiones.password, e.target, 'password');
+			validarPassword2();
+		break;
+		case "password2":
+			validarPassword2();
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+	}
 }
 
 const validarCampo = (expresion, input, campo) => {
@@ -70,6 +64,5 @@ inputs.forEach((input) => {
 	input.addEventListener('blur', validarFormulario);
 });
 
-formulario.addEventListener('submit', (e)=>{
-    e.preventDefault()
-})
+formulario.addEventListener('submit', (e) => {
+});
